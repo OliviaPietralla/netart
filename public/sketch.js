b197bf979c065f0f6c85588992f4cbad45c4f793
Olivia Pietralla <pietrallaok@gmail.com>
2023-03-24 17:20:10 +1100

    added a pink square

--- public/sketch.js
+++ public/sketch.js
@@ -3,8 +3,13 @@ document.body.style.overflow = `hidden`
 
 function setup () {
    createCanvas (innerWidth, innerHeight)
+   fill ('deeppink')
+   rectMode (CENTER)
+   noStroke ()
+   noLoop ()
 }
 
 function  draw () {
-   background (`turquoise`)
+   background (`green`)
+   square (width / 2, height / 2, 100)
 }
